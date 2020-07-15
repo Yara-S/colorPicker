82111009c01f3c5d17d24a7754f65a6fa2ae14a5
Yara-S <dsouza.yara@gmail.com>
2020-07-15 17:00:38 -0300

    Added change to green and blue. Added feature of text-color change

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
 window.addEventListener('load', start);
 
-var current_backgroud_color = "#ffffff";
+var current_backgroud_color;
+
 
 
 function start(){
 	//setting default background
 	var body = document.querySelector('body');
 	body.style.backgroundColor = "#ffffff";
+	current_backgroud_color = "#ffffff";
+	//setting default text color
+	body.style.color = "#000000";
 }
 
 //watch red_counter
 var red_range = document.querySelector('#red_range');
 red_range.addEventListener('input', changeRed);
+//watch green_counter
+var green_range = document.querySelector('#green_range');
+green_range.addEventListener('input', changeGreen);
+//watch blue_counter
+var blue_range = document.querySelector('#blue_range');
+blue_range.addEventListener('input', changeBlue);
 
 function changeRed(event){
 	var red_value = event.target.value;
@@ -20,17 +30,54 @@ function changeRed(event){
 	changeBackground('R', parseInt(red_value));
 }
 
+function changeGreen(event){
+	var green_value = event.target.value;
+	var green_counter = document.querySelector('#green_counter');
+	green_counter.value = green_value;
+	changeBackground('G', parseInt(green_value));
+}
+
+function changeBlue(event){
+	var blue_value = event.target.value;
+	var blue_counter = document.querySelector('#blue_counter');
+	blue_counter.value = blue_value;
+	changeBackground('B', parseInt(blue_value));
+}
 
 
 function changeBackground(color, quantity){
 	var body = document.querySelector('body');
 	//converting to hexadecimal
 	var hex_color = quantity.toString(16);
-	if (hex_color.lenght===1){
+	console.log(typeof(hex_color));
+	if (hex_color.length===1){
 		hex_color = "0"+ hex_color;
+		//console.log(hex_color);
 	}
+	//checking color
 	if (color === 'R'){
 		current_backgroud_color = "#" + hex_color + current_backgroud_color.slice(3,7) ;
 	}
+	else if (color === 'G'){
+		current_backgroud_color = current_backgroud_color.slice(0,3)+ hex_color+current_backgroud_color.slice(5,7);
+	}
+	else{
+		current_backgroud_color = current_backgroud_color.slice(0,5)+ hex_color;
+	}
 	body.style.backgroundColor = current_backgroud_color;
+	changeTextColor(body);
+	//console.log(current_backgroud_color);
+}
+
+function changeTextColor(body){
+	var background = parseInt(current_backgroud_color.slice(1,7), 16);
+	var contrasting_color = 16777215 - background;
+	var contrasting_color_hex = contrasting_color.toString(16);
+	while (contrasting_color_hex.length < 6){
+		contrasting_color_hex = "0"+contrasting_color_hex;
+	}
+	contrasting_color_hex = "#"+ contrasting_color_hex;
+	body.style.color = contrasting_color_hex;
+
+
 }
